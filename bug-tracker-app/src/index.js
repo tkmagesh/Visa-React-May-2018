be85550700d9df71f54c2bc5efbbcca3497b3aad
Magesh <tkmagesh77@gmail.com>
2018-05-24 16:48:52 +0530

    routing implementation

--- bug-tracker-app/src/index.js
+++ bug-tracker-app/src/index.js
@@ -7,18 +7,21 @@ import appStore from './store';
 import BugTracker from './bugTracker';
 import Spinner from './spinner';
 
-import axios from 'axios';
-
-window['axios'] = axios;
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 ReactDOM.render(
-<Provider store={appStore}>
-	<div>
-		<Spinner />
-		<hr />
-		<BugTracker />
-	</div>
-</Provider>
-,
-	document.getElementById('root'));
-
+	<Provider store={appStore}>
+		<Router>
+			<div>
+				<Link to="/">Bug Tracker</Link>
+				<br/>
+				<Link to="/spinner">Spinner</Link>
+				<br />
+			  <Route exact path="/" component={BugTracker} />
+		      <Route path="/bugs" component={BugTracker} />
+		      <Route path="/spinner" component={Spinner} />
+		     </div>
+	    </Router>
+	</Provider>
+	, document.getElementById('root')
+);
